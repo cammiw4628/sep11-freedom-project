53aa497e7ebfe98fdd98be0ed8986f4ee154bb20
Cammi <cammiw@nycstudents.net>
2025-10-28 19:35:57 -0400

    update repo

--- tool/kaboom/kaboom.js
+++ tool/kaboom/kaboom.js
@@ -1,7 +1,39 @@
-kaboom();
+kaboom({
+	background: [ 107, 176, 99 ],
+})
+
 loadSprite("green", "sprites/green-seed.png")
 add([
     sprite("green"),
     pos(120, 80),
-    scale(0.5)
+    scale(0.3),
 ]);
+loadSprite("player", "sprites/farmer.png")
+const SPEED = 320
+const player = add([
+    sprite("player"),
+    pos(145,80),
+    scale(0.5),
+    rotate(0),
+    area(),
+    body(),
+]);
+
+onKeyDown("right", () => {
+	player.move(SPEED, 0)
+})
+
+onKeyDown("left", () => {
+	player.move(-SPEED, 0)
+})
+
+
+
+
+add([
+    pos(35, 25),
+    text("cool!", {
+        size: 48,
+        width: 320,
+    })
+])

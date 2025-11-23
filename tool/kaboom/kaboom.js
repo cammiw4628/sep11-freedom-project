99c0cadb71875b10e24a4de7248be3b09c249d1c
Cammi <cammiw@nycstudents.net>
2025-11-22 21:02:53 -0500

    update repo

--- tool/kaboom/kaboom.js
+++ tool/kaboom/kaboom.js
@@ -1,8 +1,15 @@
 kaboom({
-	background: [ 107, 176, 99 ],
+	background: [ 0, 0, 0 ],
 })
+// create grass background
+loadSprite("grass", "/sprites/grass.png");
+add([
+    sprite("grass"),
+    pos(0, 0),
+    scale(5),
+]);
 
-loadSprite("green", "sprites/green-seed.png")
+loadSprite("green", "/sprites/green-seed.png");
 const greenSeed = add([
     sprite("green"),
     pos(120, 350),
@@ -12,13 +19,19 @@ const greenSeed = add([
 
 // when user mouse hovers over a sprite
 greenSeed.onHover(() => {
-    debug.log("hovering on green seed")
-    greenSeed.color = YELLOW
+    debug.log("hovering on seed")
+    greenSeed.color = GREEN
 })
+loadSprite("seed", "sprites/green-seed.png")
+add([
+    sprite("seed"),
+    pos(250, 350),
+    scale(0.3),
+]);
 // user unhovers the sprite
 greenSeed.onHoverEnd(() => {
-    debug.log("unhovered green seed")
-    greenSeed.color = GREEN
+    debug.log("unhovered seed")
+    greenSeed.color = YELLOW
 })
 
 // create player sprite
